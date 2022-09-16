d4e3feb675c916d3e5855dcbc338ad9105e5bd06
Aviraj Khare <avirajkhare00@gmail.com>
2022-09-16 19:11:04 +0530

    [Aviraj] Initial commit

---
+++ HelloWorld/Hello.rs
@@ -0,0 +1,9 @@
+fn main(){
+    // This is a single line comment
+    /*
+    This is multiline
+    comment.
+    */
+    println!("Hello. Starting to hack Rust!");
+    println!("My name is {}.", "Aviraj Khare");
+}
---
+++ HelloWorld/Variables.rs
@@ -0,0 +1,4 @@
+fn main(){
+    let my_name = "Aviraj Khare";
+    println!("My name is {}.", my_name);
+}
